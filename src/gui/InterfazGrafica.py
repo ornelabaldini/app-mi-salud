b8373f9e22f42deeaceba5fd55ef84a326026469
ornelabaldini <ornelabaldin@gmail.com>
2024-10-27 14:21:02 -0300

    enter al boton aceptar

--- src/gui/InterfazGrafica.py
+++ src/gui/InterfazGrafica.py
@@ -8,16 +8,14 @@ from src.models.dao.datos import FechaHora
 class InterfazGrafica(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title("Registro de Pacientes")
+        self.title("            Registro de pacientes y medicamentos")
         self.geometry("1000x1000")
         self.pacientes = {}
         self.fecha_hora = FechaHora()
 
-        # Frame para entrada de datos
         self.frame_entrada = ctk.CTkFrame(self, corner_radius=10, fg_color="#3a3d3f")
         self.frame_entrada.pack(pady=10, padx=10)
 
-        # Etiquetas y entradas para paciente
         self.etiqueta_nombre_paciente = ctk.CTkLabel(self.frame_entrada, text="Nombre:", font=("Arial",20))
         self.etiqueta_nombre_paciente.grid(row=0, column=0, pady=10, padx=10)
         self.entrada_nombre_paciente = ctk.CTkEntry(self.frame_entrada, corner_radius=100, width=150, height=30)
@@ -30,15 +28,12 @@ class InterfazGrafica(ctk.CTk):
         self.entrada_edad_paciente.grid(row=1, column=1, pady=10, padx=(5,5))
         self.entrada_edad_paciente.bind("<Return>", self.enfocar_siguiente_campo)
 
-        # Botón para ingresar paciente
         self.boton_ingresar_paciente = ctk.CTkButton(self.frame_entrada, text="Ingresar paciente", command=self.ingresar_paciente, font=("Arial",20))
         self.boton_ingresar_paciente.grid(row=2, column=0, columnspan=2, pady=10, padx=10)
 
-        # Frame para registro de síntomas y medicaciones
         self.frame_registro = ctk.CTkFrame(self)
         self.frame_registro.pack(pady=20, padx=10)
 
-        # Etiquetas y entradas para síntoma
         self.etiqueta_descripcion_sintoma = ctk.CTkLabel(self.frame_registro, text="Descripción del síntoma:",font=("Arial",20))
         self.etiqueta_descripcion_sintoma.grid(row=0, column=0, pady=10, padx=10)
         self.entrada_descripcion_sintoma = ctk.CTkEntry(self.frame_registro,corner_radius=100, width=200, height=30)
@@ -51,11 +46,9 @@ class InterfazGrafica(ctk.CTk):
         self.entrada_fecha_hora_sintoma.grid(row=1, column=1, pady=10,  padx=(0,0))
         self.entrada_fecha_hora_sintoma.bind("<Return>", self.enfocar_siguiente_campo)
 
-        # Botón para registrar síntoma
         self.boton_registrar_sintoma = ctk.CTkButton(self.frame_registro, text="Registrar síntoma", command=self.registrar_sintoma, font=("Arial",20))
         self.boton_registrar_sintoma.grid(row=2, column=0, columnspan=2, pady=10, padx=10)
 
-        # Etiquetas y entradas para medicación
         self.etiqueta_descripcion_medicacion = ctk.CTkLabel(self.frame_registro, text="Descripción de la medicación:", font=("Arial",20))
         self.etiqueta_descripcion_medicacion.grid(row=3, column=0, pady=10, padx=10)
         self.entrada_descripcion_medicacion = ctk.CTkEntry(self.frame_registro, corner_radius=100, width=200, height=30)
@@ -74,15 +67,12 @@ class InterfazGrafica(ctk.CTk):
         self.entrada_fecha_hora_medicacion.grid(row=5, column=1, pady=10, padx=(10,5))
         self.entrada_fecha_hora_medicacion.bind("<Return>", self.enfocar_siguiente_campo)
 
-        # Botón para registrar medicación
         self.boton_registrar_medicacion = ctk.CTkButton(self.frame_registro, text="Registrar medicación", command=self.registrar_medicacion, font=("Arial",20))
         self.boton_registrar_medicacion.grid(row=6, column=0, columnspan=2, pady=10, padx=10)
 
-        # Botón para ver registros
         self.boton_ver_registros = ctk.CTkButton(self.frame_registro, text="Ver registros", command=self.ver_registros, font=("Arial",20))
         self.boton_ver_registros.grid(row=7, column=0, columnspan=2, pady=10, padx=10)
 
-        # Botón para ver registro actual
         self.boton_ver_registro_actual = ctk.CTkButton(self.frame_registro, text="Ver registro actual",
                                                        command=self.ver_registro_actual, font=("Arial",20))
         self.boton_ver_registro_actual.grid(row=8, column=0, columnspan=2, pady=10, padx=10)
@@ -162,9 +152,9 @@ class InterfazGrafica(ctk.CTk):
 
             for registro in registros_paciente.registros:
                 if isinstance(registro, Sintoma):
-                    mensaje += f"Síntoma: {registro.descripcion} - Fecha y hora: {registro.fecha_hora}\n\n"
+                    mensaje += f"Síntoma: {registro.descripcion} - Fecha y hora de síntoma: {registro.fecha_hora}\n\n"
                 elif isinstance(registro, Medicacion):
-                    mensaje += f"Medicación: {registro.descripcion} - Dosis: {registro.dosis} - Fecha y hora: {registro.fecha_hora}\n\n"
+                    mensaje += f"Medicación: {registro.descripcion} - Dosis: {registro.dosis} - Fecha y hora de medicación: {registro.fecha_hora}\n\n"
 
             mensaje += "\n"
 
@@ -172,16 +162,27 @@ class InterfazGrafica(ctk.CTk):
 
     def ver_registro_actual(self):
         nombre_paciente = self.entrada_nombre_paciente.get()
+
         if nombre_paciente in self.pacientes:
             registros = self.pacientes[nombre_paciente].registros
+
             if registros:
-                registro = registros[-1]
-                if isinstance(registro, Sintoma):
-                    mensaje = f"Registro de {nombre_paciente}:\n\nSíntoma: {registro.descripcion} - Fecha y hora: {registro.fecha_hora}"
-                    self.mostrar_mensaje_exito(mensaje)
-                if isinstance(registro, Medicacion):
-                    mensaje = f"Registro de {nombre_paciente}:\n\nMedicación: {registro.descripcion} - Dosis: {registro.dosis} - Fecha y hora: {registro.fecha_hora}"
-                    self.mostrar_mensaje_exito(mensaje)
+                ultimo_sintoma = None
+                ultima_medicacion = None
+
+                for registro in registros:
+                    if isinstance(registro, Sintoma):
+                        ultimo_sintoma = registro
+                    elif isinstance(registro, Medicacion):
+                        ultima_medicacion = registro
+
+                mensaje = f"Registro de {nombre_paciente}:\n\n"
+                if ultimo_sintoma:
+                    mensaje += f"Fecha y hora de síntoma: {ultimo_sintoma.fecha_hora} - Síntoma: {ultimo_sintoma.descripcion}\n"
+                if ultima_medicacion:
+                    mensaje += f"Fecha y hora de medicación: {ultima_medicacion.fecha_hora} - Medicación: {ultima_medicacion.descripcion} - Dosis: {ultima_medicacion.dosis}\n"
+
+                self.mostrar_mensaje_exito(mensaje.strip())
             else:
                 self.mostrar_mensaje_error("No hay registros.")
         else:
@@ -197,11 +198,15 @@ class InterfazGrafica(ctk.CTk):
         boton_aceptar = ctk.CTkButton(ventana_mensaje, text="Aceptar", command=ventana_mensaje.destroy, font=("Arial",20))
         boton_aceptar.pack(pady=20, padx=10)
 
+  
     def mostrar_mensaje_error(self, mensaje):
         ventana_mensaje = ctk.CTkToplevel(self)
-        ventana_mensaje.title("                    Error")
+        ventana_mensaje.title("Error")
         etiqueta_mensaje = ctk.CTkLabel(ventana_mensaje, text=mensaje, font=("Arial", 20))
         etiqueta_mensaje.pack(pady=20, padx=10)
-        boton_aceptar = ctk.CTkButton(ventana_mensaje, text="Aceptar", command=ventana_mensaje.destroy,
-                                      font=("Arial", 20))
+
+        boton_aceptar = ctk.CTkButton(ventana_mensaje, text="Aceptar", command=ventana_mensaje.destroy, font=("Arial", 20))
         boton_aceptar.pack(pady=20, padx=10)
+
+        ventana_mensaje.bind("<Return>", lambda event: boton_aceptar.invoke())
+
--- src/models/dao/datos.py
+++ src/models/dao/datos.py
@@ -3,4 +3,3 @@ from datetime import datetime
 class FechaHora:
     def obtener_fecha_hora(self):
         return datetime.now().strftime("%Y-%m-%d %H:%M")
-
